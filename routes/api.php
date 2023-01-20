d96acd503c7b6c45552b7f3b6ab5d86e595b88d6
Renegade <lolade.wahab@mypatricia.co>
2023-01-20 16:32:06 +0100

    create api endpoint for sending emails

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'api',], function () {
+    Route::post('/send-email', [EmailController::class, 'sendEmail'])->name('email.send');
 });
