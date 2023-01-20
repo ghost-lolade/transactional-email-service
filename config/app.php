e3ce6026d13873c45285fb861526f3bc7bfeb1be
Renegade <lolade.wahab@mypatricia.co>
2023-01-20 19:40:13 +0100

    register mailjet service alias

--- config/app.php
+++ config/app.php
@@ -194,6 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Interfaces\EmailInterface\MailJet::class,
 
     ],
 
