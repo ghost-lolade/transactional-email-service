1475669f161539800688f981ef786884caefaa19
Renegade <lolade.wahab@mypatricia.co>
2023-01-23 12:38:01 +0100

    add docbloc to email interface

--- app/Interfaces/EmailInterfaces/Email.php
+++ app/Interfaces/EmailInterfaces/Email.php
@@ -4,5 +4,11 @@ namespace App\Interfaces\EmailInterfaces;
 
 interface Email
 {
+    /**
+     * Send mail using External service
+     * @param array $data
+     *
+     * @return void
+    */
     public function send($data);
 }
