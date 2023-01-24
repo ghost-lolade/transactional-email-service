0a4f34b1dcf16350ed1098b7176b0570112e94a7
Renegade <lolade.wahab@mypatricia.co>
2023-01-24 18:20:43 +0100

    logging cofig

--- config/logging.php
+++ config/logging.php
@@ -117,6 +117,11 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+        'email' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/email.log'),
+            'level' => 'debug',
+        ]
     ],
 
 ];
