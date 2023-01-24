b0b0742529db21b1b6ae0a2007a2c40273da1cae
Renegade <lolade.wahab@mypatricia.co>
2023-01-24 17:34:49 +0100

    create global helper functions for log activity

---
+++ app/Supports/helpers.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+if (! function_exists('log_activity')) {
+    /**
+     * Logs the activity.
+     */
+    function log_activity(int $statusCode = null, string $message, $data = [])
+    {
+        Log::info($message, $data);
+    }
+}
+
+if (! function_exists('log_error')) {
+    /**
+     * Throw an HTTP exception and log the exception.
+     */
+    function log_error(int $statusCode, string $message, Exception $exception)
+    {
+        report($exception);
+
+        throw new HttpException($statusCode, $message);
+    }
+}
